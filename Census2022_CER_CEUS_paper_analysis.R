2e7e36173b86c0f6ac9ccc588d1edb77d5ed3f5d
Ben Anderson <dataknut@icloud.com>
2016-03-14 11:20:56 +0000

    added ref to location of data pre-processing script

--- Census2022_CER_CEUS_paper_analysis.R
+++ Census2022_CER_CEUS_paper_analysis.R
@@ -1,30 +1,34 @@
 ####################################################################################
 # Data Analysis for ESRC Transformative project paper for Computers, Environment & Urban Systems
+#
 # - Using the Commission for Energy Regulation (CER)'s Irish Smart Meter Trial data
 #   - http://www.ucd.ie/issda/data/commissionforenergyregulationcer/
-
+#
 # For the latest version of this code go to: https://github.com/dataknut/Census2022
-
+#
+# Uses CER data pre-processed using:
+# https://github.com/dataknut/CER/blob/master/CER-data-processing-electricity.R
+#
 # This work was funded by RCUK through the ESRC's Transformative Social Science Programme via the
 # "Census 2022: Transforming Small Area Socio-Economic Indicators through 'Big Data'" Project 
 # - http://gtr.rcuk.ac.uk/project/2D2CD798-4F04-4399-B1AF-D810A233DD21
 # - http://www.energy.soton.ac.uk/tag/census2022/
-
+#
 # Copyright (C) 2014-2016  University of Southampton
-
+#
 # Authors: Ben Anderson (b.anderson@soton.ac.uk, @dataknut), Sharon Lin (X.Lin@soton.ac.uk) 
 #  [Energy & Climate Change, Faculty of Engineering & Environment, University of Southampton]
-
+#
 # This program is free software; you can redistribute it and/or modify it under the terms of the 
 # GNU General Public License as published by the Free Software Foundation; either version 2 of the License 
 # (http://choosealicense.com/licenses/gpl-2.0/), or (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
 # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
 # See the GNU General Public License or the LICENSE file in this repository for more details.
-
+#
 # YMMV - http://en.wiktionary.org/wiki/YMMV
-
+#
 ###########################################
 # Housekeeping ----
 # clear out all old objects etc to avoid confusion
